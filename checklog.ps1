0a177b38d20ea9b14d3b5ad5f63f62b5d6007205
Alexey A. Leonov <aaleonov@yandex.ru>
2017-05-28 20:35:38 +0300

    * guide links

--- checklog.ps1
+++ checklog.ps1
@@ -171,7 +171,7 @@ Get-Item (Join-Path $path $files) | %{
         Write-Host 'Please, check it:' http://www.yougetsignal.com/tools/open-ports/
         Write-Host
         Write-Host 'Please, read this manual to fix this:
-        https://docs.storj.io/docs/storj-share-gui-5xx#section-5-storj-share-troubleshooting
+        GUI:    https://docs.storj.io/docs/storj-share-gui-5xx#section-5-storj-share-troubleshooting
         '
     }
     if ($isTunneling) {
@@ -179,7 +179,7 @@ Get-Item (Join-Path $path $files) | %{
     }
     if ($upnp -or $isTunneling) {
         Write-Host 'Please, read this manual to fix this: 
-        https://docs.storj.io/docs/storj-share-gui-5xx#section-3-3-2-advanced-configuration
+        GUI:    https://docs.storj.io/docs/storj-share-gui-5xx#section-3-3-2-advanced-configuration
         '
     }
     if (Test-Path (Join-Path $env:TEMP ($file.BaseName + $file.Extension))) {
